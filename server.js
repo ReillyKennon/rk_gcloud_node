b884cc75bbdea554e70cd1d8e7a47ac93dac04e9
reilly_k <reilly.kennon@gmail.com>
2017-12-02 21:35:44 -0800

    successful deploy

--- server.js
+++ server.js
@@ -8,15 +8,35 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, 'rk_react_template/build')));
+
 
 // test page 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello from rk_server');
-})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'rk_react_template/build', 'index.html'));
+  });
+// app.get('/', (req, res) => {
+//     res.status(200).send('Hello from rk_server');
+// })
 
 
 // Start the server
-const server = app.listen(process.env.PORT || 7777, () => {
-    const port = server.address().port;
-    console.log(`App listening on port ${port}`);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
 });
+
+
+
+
+
+
+
+// const server = app.listen(process.env.PORT || 7777, () => {
+//     const port = server.address().port;
+//     console.log(`App listening on port ${port}`);
+// });
