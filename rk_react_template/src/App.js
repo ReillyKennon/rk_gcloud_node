044a2bf4e495a29916fe41742a5e2ca12f6a31c0
reilly_k <reilly.kennon@gmail.com>
2017-12-04 21:08:23 -0800

    add react router json packages

--- rk_react_template/src/App.js
+++ rk_react_template/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">HELLO FROM RK REACT SERVER!!</h1>
         </header>
         <p className="App-intro">
           If you are reading me I'm working!
         </p>
+        <p>Some new content</p>
       </div>
     );
   }
